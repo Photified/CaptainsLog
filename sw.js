f087fb0a9b9df4933e93051f7fee1551b713edf4
Photified <justinmainphoto@gmail.com>
2026-05-12 02:20:26 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'lcars-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  'https://unpkg.com/react@18/umd/react.development.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.development.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js'
+];
+
+// Install Event: Cache essential files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Fetch Event: Serve from cache if offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Return cached version
+        }
+        return fetch(event.request); // Otherwise fetch from network
+      })
+  );
+});
+
+// Message Event: Listen for Alarm Pings from React to show Notification
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SHOW_NOTIFICATION') {
+    self.registration.showNotification(event.data.title, {
+      body: event.data.body,
+      icon: 'icon-192.png',
+      vibrate: [300, 100, 300]
+    });
+  }
+});
